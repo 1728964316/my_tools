5d2f89c7089b12e71bd380241ef26948b19bc01f
dioss <92138704+1728964316@users.noreply.github.com>
2023-08-15 23:48:55 +0800

    Update main.py

--- demo1/main.py
+++ demo1/main.py
@@ -1,47 +1,89 @@
+import os
+
 import httpx
 from lxml import etree
 
-base_url = "https://www.zhonghuadiancang.com/waiguomingzhu/9768/"
+# 设置默认输出格式为txt
+form = "txt"
+# 标题变量,用来存储小说名称
+title = ""
 
 
-def interview(pg, index):
-    # Get the response from the URL
-    response = httpx.get(
-        "https://www.zhonghuadiancang.com/waiguomingzhu/9768/" + pg + ".html"
-    )
-    # Parse the HTML and extract the content
+# 爬取页面内容的函数
+def interview(url, pg):
+    global title, form
+
+    # 获取页面内容
+    response = httpx.get(url)
+    # 将页面解析为HTML结构
     root = etree.HTML(response.text)
-    content = root.xpath("/html/body/div[2]/div[2]/div/div/div[2]/h1/text()")
-    # Iterate through the content
-    for i in content:
-        # Print the content and complete the file
-        print(i, "完成")
-        with open("text" + index + ".txt", "a", encoding="utf-8") as f:
-            f.write(i + "\n")
 
-    # Extract the content from the HTML
+    # 从页面中提取标题
+    pg_title = root.xpath("/html/body/div[2]/div[2]/div/div/div[2]/h1/text()")[0]
+
+    # 打印爬取的章节名称和页数
+    print("{}完成 (第{}页)".format(pg_title, pg))
+
+    # 将标题和内容写入文件
+    with open(
+        "./{}/{}.{}".format(title, pg_title.strip(), form), "a", encoding="utf-8"
+    ) as f:
+        f.write(pg_title + "\n")
+
+    # 提取正文内容
     content = root.xpath('//*[@id="content"]/p/text()')
     t = ""
-    # Iterate through the content
     for i in content:
-        # Append the content to the text file
-        t += "  " + i + "\n"
-    # Write the content to the text file
-    with open("text" + index + ".txt", "a", encoding="utf-8") as f:
-        f.write(t + "\t")
+        t += "  " + i.strip()
+
+    # 将正文内容写入文件
+    with open(
+        "./{}/{}.{}".format(title, pg_title.strip(), form), "a", encoding="utf-8"
+    ) as f:
+        f.write(t)
+
+
+# 计算小说章节数的函数
+def count_pg(url):
+    global title
+
+    # 获取主页内容
+    response = httpx.get(url)
+    root = etree.HTML(response.text)
+
+    # 提取小说书名作为目录名
+    title = root.xpath("/html/body/div[2]/div[2]/div[1]/div/div[2]/h1/text()")[
+        0
+    ].strip()
+    print("开始爬取", title)
+
+    # 创建书名目录
+    if not os.path.exists("./{}".format(title)):
+        os.makedirs("./{}".format(title))
+
+    # 提取所有章节链接
+    result = root.xpath("//*[@id='booklist']/li/a/@href")
+    print("一共", len(result), "页")
+    return result
 
 
 def start():
-    # Initialize the index
-    index = 0
-    # Iterate through the pages
-    for i in range(199584, 199628):
-        # Call the interview function
-        interview(str(i), str(int(index / 5)))
-        # Increment the index
+    global form
+    # 用户输入输出格式
+    flag = input("输出格式(默认txt,回车跳过):")
+    if flag != "":
+        form = flag
+
+    # 用户输入小说网址
+    url = input("请输入网址: ")
+    pg_url_list = count_pg(url)
+
+    # 遍历所有章节链接
+    index = 1
+    for i in pg_url_list:
+        interview(i, str(index))
         index += 1
 
 
 if __name__ == "__main__":
-    # Start the program
     start()
